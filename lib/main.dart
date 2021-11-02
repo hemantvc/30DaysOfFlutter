7f43701968abe5d75bd761a27d896094ec8409b1
Hemant <hemant@unoindia.co>
2021-11-02 17:52:50 +0530

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import "package:flutter/material.dart";
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Material(
+        child: Center(
+            child: Container(child: Text("Welcome to 30 days of flutter!"))),
+      ),
+    );
+  }
+}
