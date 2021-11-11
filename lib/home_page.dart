6913eed66344361c9942a82730f411b6d0d5bc5d
Hemant <hemant@unoindia.co>
2021-11-11 18:14:25 +0530

    flutter_day_2

---
+++ lib/home_page.dart
@@ -0,0 +1,24 @@
+import "package:flutter/material.dart";
+
+// ignore: must_be_immutable
+class HomePage extends StatelessWidget {
+  int days = 30;
+  String name = "Dhimahi Hemant";
+
+  HomePage({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Catelog App"),
+      ),
+      drawer: Drawer(),
+      body: Center(
+          child: Container(
+              child: Text(
+        "Welcome to $days days of flutter! $name",
+        style: const TextStyle(fontSize: 20),
+      ))),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,17 @@
 import "package:flutter/material.dart";
+import "home_page.dart";
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Material(
-        child: Center(
-            child: Container(child: Text("Welcome to 30 days of flutter!"))),
-      ),
+      home: HomePage(),
     );
   }
 }
