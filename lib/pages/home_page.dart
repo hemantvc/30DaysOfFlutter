c7b94651620dd1e556a6e6c2665bfada6ad6e60d
Hemant <hemant@unoindia.co>
2021-11-30 17:30:05 +0530

    Day 12 completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,7 +1,8 @@
 import "package:flutter/material.dart";
+import 'package:flutter_day_1/models/catelog.dart';
+import 'package:flutter_day_1/widgets/item_widget.dart';
 import 'package:flutter_day_1/widgets/drawer.dart';
 
-// Day 11 we learn context and constrain
 // ignore: must_be_immutable
 class HomePage extends StatelessWidget {
   int days = 30;
@@ -10,16 +11,19 @@ class HomePage extends StatelessWidget {
   HomePage({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
+    final dummyList = List.generate(50, (index) => CatelogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
         title: const Text("Catelog App"),
       ),
-      body: Center(
-          child: Container(
-              child: Text(
-        "Welcome to $days days of flutter! $name",
-        style: const TextStyle(fontSize: 20),
-      ))),
+      body: ListView.builder(
+        itemCount: dummyList.length,
+        itemBuilder: (context, index) {
+          return ItemWidget(
+            item: dummyList[index],
+          );
+        },
+      ),
       drawer: MyDrawer(),
     );
   }
