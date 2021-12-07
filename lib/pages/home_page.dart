5bfc9026eeb79e92845d8b653f6b515541fe0b64
Hemant <hemant@unoindia.co>
2021-12-07 12:44:41 +0530

    day 16 completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,28 +1,57 @@
 import "package:flutter/material.dart";
+import 'package:flutter/services.dart';
+import 'dart:convert';
 import 'package:flutter_day_1/models/catelog.dart';
 import 'package:flutter_day_1/widgets/item_widget.dart';
 import 'package:flutter_day_1/widgets/drawer.dart';
 
 // ignore: must_be_immutable
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   int days = 30;
+
   String name = "Dhimahi Hemant";
 
-  HomePage({Key? key}) : super(key: key);
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    await Future.delayed(Duration(seconds: 2));
+    final catelogJson =
+        await rootBundle.loadString("assets/files/catelog.json");
+    final decodeData = jsonDecode(catelogJson);
+    var productData = decodeData["products"];
+    CatelogModel.items =
+        List.from(productData).map<Item>((item) => Item.fromMap(item)).toList();
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
-    final dummyList = List.generate(50, (index) => CatelogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
         title: const Text("Catelog App"),
       ),
-      body: ListView.builder(
-        itemCount: dummyList.length,
-        itemBuilder: (context, index) {
-          return ItemWidget(
-            item: dummyList[index],
-          );
-        },
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: (CatelogModel.items != null && CatelogModel.items.isNotEmpty)
+            ? ListView.builder(
+                itemCount: CatelogModel.items.length,
+                itemBuilder: (context, index) =>
+                    ItemWidget(item: CatelogModel.items[index]),
+              )
+            : Center(
+                child: CircularProgressIndicator(),
+              ),
       ),
       drawer: MyDrawer(),
     );
